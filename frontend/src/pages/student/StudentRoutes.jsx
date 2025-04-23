d0b883a307ac8444e9e9e9d79e4f5617807f1fb4
Srujan S P <srujansjapthi@gmail.com>
2025-04-23 23:36:58 +0530

    Student Page UI Design

--- frontend/src/pages/student/StudentRoutes.jsx
+++ frontend/src/pages/student/StudentRoutes.jsx
@@ -3,13 +3,14 @@ import ProtectStudentRoute from "./auth/ProtectStudentRoute";
 import StudentLogin from "./login/StudentLogin";
 import AppLayout from "@/layouts/AppLayout";
 import NotFound from "../not-found/NotFound";
+import StudentDashboard from "./dashboard/StudentDashboard";
 
 export default function StudentRoutes() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
         <Route element={<ProtectStudentRoute />}>
-          <Route path="profile" element={<NotFound />} />
+          <Route path="dashboard" element={<StudentDashboard />} />
         </Route>
       </Route>
 
