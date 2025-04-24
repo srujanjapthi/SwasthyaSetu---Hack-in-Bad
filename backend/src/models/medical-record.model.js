46800acf0bc8c1c5d1e033dd4cb9248468907c52
Srujan S P <srujansjapthi@gmail.com>
2025-04-24 18:45:02 +0530

    Medical Record Schema Modification

--- backend/src/models/medical-record.model.js
+++ backend/src/models/medical-record.model.js
@@ -3,6 +3,8 @@ import { model, Schema } from "mongoose";
 const medicalRecordSchema = new Schema(
   {
     student_id: { type: Schema.Types.ObjectId, ref: "Student", required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
     record_url: { type: String, required: true },
   },
   { timestamps: true }
